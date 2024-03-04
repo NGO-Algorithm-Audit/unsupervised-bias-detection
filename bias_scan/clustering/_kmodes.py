ac92fb391e440a1c2926b384342daa3591858dfd
Krsto Proroković <krstopro@macbook-pro.t-com.me>
2024-03-04 15:05:22 +0100

    Update CI, refactor source

--- bias_scan/clustering/_kmodes.py
+++ bias_scan/clustering/_kmodes.py
@@ -11,29 +11,14 @@ class BiasAwareHierarchicalKModes(BiasAwareHierarchicalClustering):
         Maximum number of iterations.
     min_cluster_size : int
         Minimum size of a cluster.
-    init : {'Huang', 'Cao', 'random'}, default='Cao'
-    n_init : int, default=10
-    kmodes_max_iter : int, default=100
+    kmodes_params : dict
+        k-modes parameters
     """
 
-    def __init__(
-        self,
-        max_iter,
-        min_cluster_size,
-        init="Cao",
-        n_init=10,
-        kmodes_max_iter=100,
-    ):
+    def __init__(self, max_iter, min_cluster_size, kmodes_params={"n_clusters": 2}):
         super().__init__(max_iter, min_cluster_size)
-        self.init = init
-        self.n_init = n_init
-        self.kmodes_max_iter = kmodes_max_iter
+        self.kmodes_params = kmodes_params
+        self.kmodes = KModes(**kmodes_params)
 
-    def split(self, X):
-        kmodes = KModes(
-            n_clusters=2,
-            init=self.init,
-            n_init=self.n_init,
-            max_iter=self.kmodes_max_iter,
-        )
-        return kmodes.fit_predict(X)
+    def _split(self, X):
+        return self.kmodes.fit_predict(X)
--- tests/test_bahc.py
+++ tests/test_bahc.py
@@ -3,11 +3,21 @@ import numpy as np
 from bias_scan.clustering import BiasAwareHierarchicalKMeans
 
 
+def test_shapes():
+    # Checks that labels and biases have the right shapes
+    rng = np.random.RandomState(12)
+    X = rng.rand(20, 10)
+    y = rng.rand(20)
+    algo = BiasAwareHierarchicalKMeans(max_iter=5, min_cluster_size=2)
+    algo.fit(X, y)
+    assert len(algo.labels_) == 20
+    assert len(algo.biases_) == algo.n_clusters_
+
 def test_clusters():
     # Checks that label values are between 0 and n_clusters
     rng = np.random.RandomState(12)
-    X = rng.rand(10, 5)
-    y = rng.rand(10)
-    algo = BiasAwareHierarchicalKMeans(max_iter=3, min_cluster_size=2)
+    X = rng.rand(20, 10)
+    y = rng.rand(20)
+    algo = BiasAwareHierarchicalKMeans(max_iter=5, min_cluster_size=2)
     algo.fit(X, y)
     assert np.array_equal(np.unique(algo.labels_), np.arange(algo.n_clusters_))
