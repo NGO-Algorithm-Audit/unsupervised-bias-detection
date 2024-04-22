be313b1dd9da001a5351fbc9c1159f92559b18ec
Krsto Proroković <krstopro@macbook-pro.t-com.me>
2024-04-22 22:08:48 +0200

    Add test_bahc.py

--- tests/test_bahc.py
+++ tests/test_bahc.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from bias_detection_tool.clustering import BiasAwareHierarchicalKMeans
+from unsupervised_bias_detection.clustering import BiasAwareHierarchicalKMeans
 
 
 def test_shapes():
@@ -8,25 +8,25 @@ def test_shapes():
     rng = np.random.RandomState(12)
     X = rng.rand(20, 10)
     y = rng.rand(20)
-    algo = BiasAwareHierarchicalKMeans(n_iter=5, min_cluster_size=2)
-    algo.fit(X, y)
-    assert len(algo.labels_) == len(X)
-    assert len(algo.biases_) == algo.n_clusters_
+    hbac = BiasAwareHierarchicalKMeans(n_iter=5, min_cluster_size=2)
+    hbac.fit(X, y)
+    assert len(hbac.labels_) == len(X)
+    assert len(hbac.scores_) == hbac.n_clusters_
 
 def test_labels():
     # Checks that label values are between 0 and n_clusters
     rng = np.random.RandomState(12)
     X = rng.rand(20, 10)
     y = rng.rand(20)
-    algo = BiasAwareHierarchicalKMeans(n_iter=5, min_cluster_size=2)
-    algo.fit(X, y)
-    assert np.array_equal(np.unique(algo.labels_), np.arange(algo.n_clusters_))
+    hbac = BiasAwareHierarchicalKMeans(n_iter=5, min_cluster_size=2)
+    hbac.fit(X, y)
+    assert np.array_equal(np.unique(hbac.labels_), np.arange(hbac.n_clusters_))
 
 def test_biases():
     # Checks that biases are sorted in descending order
     rng = np.random.RandomState(12)
     X = rng.rand(20, 10)
     y = rng.rand(20)
-    algo = BiasAwareHierarchicalKMeans(n_iter=5, min_cluster_size=2)
-    algo.fit(X, y)
-    assert np.all(algo.biases_[:-1] >= algo.biases_[1:])
+    hbac = BiasAwareHierarchicalKMeans(n_iter=5, min_cluster_size=2)
+    hbac.fit(X, y)
+    assert np.all(hbac.scores_[:-1] >= hbac.scores_[1:])
