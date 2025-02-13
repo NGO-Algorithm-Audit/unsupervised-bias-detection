2a6377f50ebd3184ecd42a4d516242955bee059c
fholstege <31654652+fholstege@users.noreply.github.com>
2025-02-13 08:35:37 +0100

    changes to lib

--- unsupervised_bias_detection/clustering/_bahc.py
+++ unsupervised_bias_detection/clustering/_bahc.py
@@ -2,7 +2,10 @@ import numpy as np
 import heapq
 from abc import ABC, abstractmethod
 from sklearn.base import BaseEstimator, ClusterMixin
-
+from scipy.stats import ttest_ind
+from scipy import stats
+from sklearn.metrics import calinski_harabasz_score as CH_score
+from mpmath import mp
 
 class BiasAwareHierarchicalClustering(ABC, BaseEstimator, ClusterMixin):
     """
@@ -20,34 +23,8 @@ class BiasAwareHierarchicalClustering(ABC, BaseEstimator, ClusterMixin):
         self.n_iter = n_iter
         self.min_cluster_size = min_cluster_size
 
-    def calc_centroids(self, X, labels):
-        """ Calculate the centroids of the clusters based on the labels
-
-        Parameters
-        ----------
-        X : array-like of shape (n_samples, n_features)
-            List of n_features-dimensional data points. Each row
-            corresponds to a single data point.
-        labels : array-like of shape (n_samples)
-            Cluster labels for each point.
-        
-        """
-
-        # create an array of (d, k) with d being the number of features and k the number of unique labels
-        centroids = np.zeros((X.shape[1], len(np.unique(labels))))
-
-        # iterate over the labels
-        for i, label in enumerate(np.unique(labels)):
-
-            # get the data points that belong to the cluster with the current label
-            X_label = X[labels == label]
-
-            # calculate the mean of the data points
-            centroids[:, i] = np.mean(X_label, axis=0)
-
-        return centroids
-
-    def fit(self, X, y):
+    
+    def fit(self, X, y, random_state=None):
         """Compute bias-aware hierarchical clustering.
 
         Parameters
@@ -129,50 +106,10 @@ class BiasAwareHierarchicalClustering(ABC, BaseEstimator, ClusterMixin):
         self.centroids_ = self.calc_centroids(X, self.labels_)
 
         return self
-    
-    def predict(self, X):
-        """Predict the cluster labels for the samples in X.
 
-        Parameters
-        ----------
-        X : array-like of shape (n_samples, n_features)
-            List of n_features-dimensional data points. Each row
-            corresponds to a single data point.
-
-        Returns
-        -------
-        labels : array-like of shape (n_samples,)
-            Cluster labels for each point.
-        """
-
-        # Validate the data
-        X = self._validate_data(
-            X, reset=False, accept_large_sparse=False, dtype=self._dtype, order="C"
-        )
-       # Get dimensions
-        n_samples = X.shape[0]
-        n_clusters = self.centroids_.shape[1]
-        
-        # Initialize distance matrix
-        distances = np.zeros((n_samples, n_clusters))
-        
-        # Compute squared Euclidean distance between each sample and each centroid
-        for k in range(n_clusters):
-
-            # Deduct the centroid from the data
-            diff = X - self.centroids_[:, k].T
-
-            # Calculate the sum of squared differences
-            distances[:, k] = np.sum(diff * diff, axis=1)
-        
-        # Get the index (label) of the closest centroid for each sample
-        labels = np.argmin(distances, axis=1)
-        
-        
-        return labels
 
     @abstractmethod
-    def _split(self, X):
+    def _split(self, X, random_state=None):
         """Split the data into two clusters.
 
         Parameters
@@ -186,3 +123,151 @@ class BiasAwareHierarchicalClustering(ABC, BaseEstimator, ClusterMixin):
             that the point belongs to the first or the second cluster, respectively.
         """
         pass
+
+    def binary_chi_square_test(self, m, labels, k, bonf_correct):
+        """
+        Performs a chi-square test of independence for two binary samples.
+        
+        Parameters:
+        -----------
+        m: np.array
+            The metric values
+        labels: np.array
+            The cluster labels
+        k: int
+            The cluster number
+        bonf_correct: bool
+            Whether to apply Bonferroni correction
+            
+        Returns:
+        --------
+            - p-value
+            - difference in proportions
+            - proportion of 1s in cluster 1
+            - proportion of 1s in cluster 0
+        """
+
+        # take the idx where labels == k
+        idx = (labels == k)
+
+        # calculate the n_clust
+        n_clust = len(np.unique(labels))
+
+        # get the target values for the two clusters
+        m1, m0 = m[idx], m[~idx]
+
+        # Input validation
+        if not all(np.isin(m, [0, 1])):
+            raise ValueError("All values must be binary (0 or 1)")
+            
+        # Create contingency table
+        a = np.sum(m1 == 1)  # count of 1s in y
+        b = np.sum(m0 == 1)  # count of 1s in z
+        c = np.sum(m1 == 0)  # count of 0s in y
+        d = np.sum(m0 == 0)  # count of 0s in z
+        
+        contingency_table = np.array([[a, b],
+                                    [c, d]])
+        
+        # Perform chi-square test
+        test_statistic, p_clust, dof = stats.chi2_contingency(contingency_table)[:3]
+       
+
+        if bonf_correct and n_clust > 2:
+            p_clust = p_clust * n_clust
+        
+        # calc difference in proportions
+        p1 = a / (a + c)
+        p0 = b / (b + d)
+        diff_clust = p1 - p0
+        
+        return p_clust, diff_clust, p1, p0
+
+
+    def t_test(self, m, labels, k, bonf_correct, alternative='two-sided'):
+
+        # take the idx where labels == k
+        idx = (labels == k)
+
+        # calculate the n_clust
+        n_clust = len(np.unique(labels))
+
+        # get the target values for the two clusters
+        m1, m0 = m[idx], m[~idx]
+    
+        # calculate the p-value using a t-test
+        t, p_clust = ttest_ind(m1, m0, equal_var=False, alternative=alternative)
+
+        # if the p-value is 0, we have numerical underflow
+        if p_clust == 0:
+
+            t = mp.mpf(t)
+            nu = mp.mpf(res.df)
+            x2 = nu / (t**2 + nu)
+            p_clust = mp.betainc(nu/2, mp.one/2, x2=x2, regularized=True)
+            print('original p is zero, accounting for underflow')
+
+        # apply bonferroni correction
+        if bonf_correct and n_clust > 2: # if there are two clusters, just use the single p-value
+            p_clust = p_clust * n_clust # Bonferroni correction - multiply by number of clusters
+
+        # calculate the difference in means
+        diff_clust = m1.mean() - m0.mean() # Difference in means
+        
+        return p_clust, diff_clust, m1.mean(), m0.mean()
+    
+
+    def calc_ratio_within_between(self, m, labels):
+
+        # loop over each cluster, record within cluster var, average, and number of samples
+        within = []
+        averages = []
+        n_k = []
+
+
+        # calculate the total number of samples and the number of clusters
+        n = len(m)
+        K = len(np.unique(labels))
+        overall_mean = np.mean(m)
+
+        # loop over each cluster
+        for k in np.unique(labels):
+
+            # take the idx where labels == k
+            idx = (labels == k)
+
+            # get m when for cluster k, calculate the mean
+            m_k = m[idx]
+            mean_k = np.mean(m_k)
+
+            # calculate the within cluster variance
+            within_k  = np.sum((m_k - mean_k)**2)
+
+            # append to the list
+            within.append(within_k)
+
+            # append the average and the number of samples
+            averages.append(mean_k)
+            n_k.append(len(m_k))
+        
+        # calculate the between cluster variance
+        B = np.sum([n_k[i]*(averages[i] - overall_mean)**2 for i in range(len(averages))])
+        B_adj = B / (K - 1) # divide between degrees of freedom
+
+        # calculate the within cluster variance
+        W = np.sum(within)
+        W_adj = W / (n - K) # divide within degrees of freedom
+
+        # calculate the ratio
+        ratio = B_adj / W_adj
+
+        return ratio, B, W
+
+        
+
+
+        
+
+
+
+            
--- unsupervised_bias_detection/clustering/_kmeans.py
+++ unsupervised_bias_detection/clustering/_kmeans.py
@@ -66,3 +66,79 @@ class BiasAwareHierarchicalKMeans(BiasAwareHierarchicalClustering):
 
     def _split(self, X):
         return self.kmeans.fit_predict(X)
+    
+
+    def calc_centroids(self, X, labels):
+        """ Calculate the centroids of the clusters based on the labels
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            List of n_features-dimensional data points. Each row
+            corresponds to a single data point.
+        labels : array-like of shape (n_samples)
+            Cluster labels for each point.
+        
+        """
+
+        # create an array of (d, k) with d being the number of features and k the number of unique labels
+        centroids = np.zeros((X.shape[1], len(np.unique(labels))))
+
+        # iterate over the labels
+        for i, label in enumerate(np.unique(labels)):
+
+            # get the data points that belong to the cluster with the current label
+            X_label = X[labels == label]
+
+            # calculate the mean of the data points
+            centroids[:, i] = np.mean(X_label, axis=0)
+
+        return centroids
+    
+    def predict(self, X):
+        """Predict the cluster labels for the samples in X.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            List of n_features-dimensional data points. Each row
+            corresponds to a single data point.
+
+        Returns
+        -------
+        labels : array-like of shape (n_samples,)
+            Cluster labels for each point.
+        """
+
+        # Validate the data
+        X = self._validate_data(
+            X, reset=False, accept_large_sparse=False, dtype=self._dtype, order="C"
+        )
+        # Get dimensions
+        n_samples = X.shape[0]
+        n_clusters = self.centroids_.shape[1]
+        
+        # Initialize distance matrix
+        distances = np.zeros((n_samples, n_clusters))
+        
+        # Compute squared Euclidean distance between each sample and each centroid
+        for k in range(n_clusters):
+
+            # get the centroid
+            centroid_k = self.centroids_[:, k].T
+
+            # Deduct the centroid from the data
+            diff = X - centroid_k
+
+            # Calculate the sum of squared differences
+            distances[:, k] = np.sum(diff * diff, axis=1)
+        
+        # Get the index (label) of the closest centroid for each sample
+        labels = np.argmin(distances, axis=1)
+        
+        
+        return labels
+
+    
+
+    
